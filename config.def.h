edf37a77e4fefbe09a9151089177766b910f2bb3
Steven Hall <Hallzy.18@gmail.com>
2020-12-05 11:31:47 -0800

    Added some new scroll mappings

--- config.def.h
+++ config.def.h
@@ -10,7 +10,10 @@ struct rule rules[] = {
 	/* sequence     event        lines */
 	{"\033[5;2~",   SCROLL_UP,   -1},       /* [Shift] + [PageUP] */
 	{"\033[6;2~",   SCROLL_DOWN, -1},       /* [Shift] + [PageDown] */
-	/* mouse binding shadows ^E and ^Y, so it's disabled by default */
-	//{"\031",        SCROLL_UP,    1},       /* mouse wheel up */
-	//{"\005",        SCROLL_DOWN,  1},       /* mouse wheel Down */
+	{"\031",        SCROLL_UP,    1},       /* mouse wheel up */
+	{"\005",        SCROLL_DOWN,  1},       /* mouse wheel Down */
+	{"\033k",       SCROLL_UP,  1},         /* Alt+k scroll 1 line up */
+	{"\033j",       SCROLL_DOWN,  1},       /* Alt+j scroll 1 line down */
+	{"\033K",       SCROLL_UP,  -2},        /* Alt+shift+k scroll half screen up */
+	{"\033J",       SCROLL_DOWN,  -2},      /* Alt+shift+j scroll half screen down */
 };
